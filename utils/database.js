bdd632a7c7bf93e29e01eadb7af36d2d9b794e60
Filipe Graça <filipegraca1992@gmail.com>
2019-06-30 01:01:55 +0100

    use env variables

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize")
 
-const sequelize = new Sequelize("node-complete", "root", "notasecret123", {
+const sequelize = new Sequelize("node-complete", process.env.DB_USER, process.env.DB_PASSWORD, {
   dialect: "mysql",
   host: "localhost",
   logging: false
